f75543ee8c8a8349adfc6b04370cc16918b5f8d4
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-05-30 11:56:52 +0900

    feat: TaskProviderをapp/layout.tsxにインポート

--- study-tracker/app/layout.tsx
+++ study-tracker/app/layout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from "react";
 import "./globals.css";
+import { TaskProvider } from "@/context/TaskContext";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-black text-white">
+        <TaskProvider>{children}</TaskProvider>
+      </body>
     </html>
   );
 }
